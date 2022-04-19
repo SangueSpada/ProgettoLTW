2e0bcbd6d3ca32e6984e31b7dbac0cb66f2cbeaa
fracchiooo <sconocchiapisoni.1889241@studenti.uniroma1.it>
2022-04-19 17:26:41 +0200

    get /titolo fatto

--- server.js
+++ server.js
@@ -5,14 +5,15 @@ var url = require('url');
 //var math = require("mathjs");
 var express = require('express');
 var app = express();
-//var bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 var fs = require('fs');
 var path = require('path');
 var port = 3334;
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname));
 
-var database=JSON.parse(fs.readFileSync('offerte.json')); //legge il contenuto di offerte json
+var database = JSON.parse(fs.readFileSync('offerte.json')); //legge il contenuto di offerte json
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 //  res.sendFile(path.join(__dirname, '/offerte.html')); per la visualizzazione statica
 
@@ -29,11 +30,13 @@ app.get('/offerte', function(req, res) {
 
 });
 
-app.get('/titolo', function(req, res) {
-    console.log('get /titolo');
-      res.render('offerte.ejs', { data: database });
+app.get('/offerta', urlencodedParser, function(req, res) {
+    //console.log(req.query.id);
+    console.log('get /offerta');
+    res.render('titolo.ejs', { data: database, id: req.query.id });
 
 });
+
 
 
 
