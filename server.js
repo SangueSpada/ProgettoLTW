d2ff64266bae7c62c91b1b0127ba5df532b8045f
SangueSpada <kankilic.1888465@studenti.uniroma1.it>
2022-04-23 19:49:54 +0200

    cookies quasi sistemati= inserimento di timeout opportuno

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ var fs = require('fs');
 var path = require('path');
 
 const { response } = require('express');
+const { md5 } = require('pg/lib/utils');
 var port = 3334;
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname)); //per elaborare il css
@@ -35,21 +36,19 @@ app.get('/', function(req, res) {
     console.log('get /');
     var cookies = cookie.parse(req.headers.cookie || '');
     var email_cookie = cookies.email_cookie;
-    //    console.log(String(req.body));
-
+    var profilo_cookie= cookies.profile_cookie;
+    console.log(email_cookie+' '+profilo_cookie);
 
 
     if (email_cookie) {
+        console.log("mando i cookie");
+        res.render('index.ejs', { data: database, cookie:email_cookie, profilo:profilo_cookie });
 
-    } else {}
-
-
-
-
-
-
-    res.render('index.ejs', { data: database });
-
+    } 
+    else {
+        console.log("non mando i cookie");
+        res.render('index.ejs', { data: database, profilo: '' });
+    }
 });
 
 
@@ -58,13 +57,15 @@ app.get('/offerte', function(req, res) {
     var cookies = cookie.parse(req.headers.cookie || '');
 
     var email_cookie = cookies.email_cookie;
+    var profilo_cookie= cookies.profile_cookie;
 
-
-    if (email_cookie) {} else {}
-
-
-    console.log('get /offerte');
-    res.render('offerte.ejs', { data: database });
+    if (email_cookie) {
+        console.log("mando i cookie");
+        res.render('offerte.ejs', { data: database, cookie:email_cookie, profilo:profilo_cookie });
+    } else {
+        console.log("non mando i cookie");
+        res.render('offerte.ejs', { data: database, profilo: '' });
+    }
 
 });
 
@@ -100,14 +101,13 @@ app.get('/profilo', urlencodedParser, function(req, res) {
     var cookies = cookie.parse(req.headers.cookie || '');
 
     var email_cookie = cookies.email_cookie;
-
+    console.log(email_cookie);
     if (email_cookie) {
 
         var n;
         var c;
         var s;
         var p;
-
 
         client.query('select * from utente where email=\'' + email_cookie + '\'', function(error, result) {
             if (error) { console.log(error); return; }
@@ -132,50 +132,58 @@ app.post('/login', urlencodedParser, function(req, res) {
 
     console.log('post /login');
     var mail = req.body.mail;
-    var password = req.body.password;
-
+    var password = md5(req.body.password);
+    var profilo;
     client.query('select * from utente where email=' + '\'' + mail + '\'' + ' and password=' + '\'' + password + '\'', function(error, result) {
 
-
         if (error) { console.log(error); return; }
-        //  console.log(result);
-        // console.log(result.rows[0].nome + result.rows[0].storico_offerte + result.rows[0].foto_profilo + result.rows[0].cognome);
+
+        console.log("Utente loggato correttamente");
+          try {
+                profilo = String(result.rows[0].foto_profilo);
+                console.log(profilo);
+                //res.render('index.ejs', { data: database, profilo: p });
+    
+            } catch (error) {
+                console.log(error);
+            }
+        
 
     });
+    setTimeout(function(){
+        //qua setto i cookie
+        //var query = url.parse(req.url, true, true).query;
+        var email_cookie;
+        var profile_cookie;
+        //    console.log(String(req.body));
+        try {
+            email_cookie = req.body.email_cookie;
+            profile_cookie = req.body.profile_cookie;
+            console.log(email_cookie+' '+profile_cookie);
+        } catch (error) {
+            console.log(error);
+        }
 
-
-
-
-
-    //qua setto i cookie
-    //var query = url.parse(req.url, true, true).query;
-    var email_cookie;
-    //    console.log(String(req.body));
-    try {
-
-        email_cookie = req.body.email_cookie;
-
-    } catch (error) {}
-
-    if (!email_cookie) {
-
-        // Set a new cookie with the name
-        res.setHeader('Set-Cookie', cookie.serialize('email_cookie', mail, {
-            httpOnly: true,
-            maxAge: 60 * 60 // 1 hour
-        }));
-
-        // Redirect back after setting cookie
-        res.statusCode = 302;
-        res.setHeader('Location', req.headers.referer || '/');
-
+        if (!email_cookie) {
+            console.log("Procedo a settare i cookies: "+mail+ ' '+profilo);
+            // Set a new cookie with the name
+            res.setHeader('Set-email-Cookie', cookie.serialize('email_cookie', mail, {
+                                                                                httpOnly: true,
+                                                                                maxAge: 60 * 60 // 1 hour
+                                                                                }));
+            res.setHeader('Set-profile-Cookie', cookie.serialize('profile_cookie', profilo, {
+                                                                                httpOnly: true,
+                                                                                maxAge: 60 * 60 // 1 hour
+                                                                                }));
+            // Redirect back after setting cookie
+            res.statusCode = 302;
+            res.setHeader('Location', req.headers.referer || '/');
+            console.log("Cookies settati");
+        }
         res.end();
-
         return;
-    }
 
-
-
+    }, 500 );
 });
 
 app.post('/signin', urlencodedParser, function(req, res) {
@@ -185,7 +193,7 @@ app.post('/signin', urlencodedParser, function(req, res) {
     var nome = req.body.firstName;
     var cognome = req.body.lastName;
     var mail = req.body.email;
-    var pass = req.body.password;
+    var pass = md5(req.body.password);
     
 
     client.query('insert into utente(email,password,storico_offerte,foto_profilo,nome,cognome) values (' + '\'' + mail + '\',' + '\'' + pass + '\',' + '\'{}\',' + '\'' + String('https://cdn.calciomercato.com/images/2019-05/Whatsapp.senza.immagine.2019.1400x840.jpg') + '\',' + '\'' + nome + '\',' + '\'' + cognome + '\');', function(error, result) {
@@ -193,32 +201,22 @@ app.post('/signin', urlencodedParser, function(req, res) {
         if (error) {
 
             if (error.code === '23505') {
-                
 
-                res.send('<p>mail gia presa </p> ');
+                res.send("<p>mail gia presa clicca <a href='/'>qui<a> per tornare all'homepage </p> ");
                 res.end();
 
-
                 // res.sendFile(path.join(__dirname, '/sigin.html'));
-
-
-
             } else {
                 throw error;
             }
         }
+        else{
+            res.send("<p>Registrazione eseguita correttamente, clicca <a href='/'>qui<a> per tornare all'homepage </p> ");
+        }
 
     });
 
-    res.render('index.ejs', { data: database });
-
-
 });
-
-
-
-
-
 
 
 
