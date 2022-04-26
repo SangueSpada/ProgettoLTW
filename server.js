aba5968b243af41cdae8bd731d05ff6b8b824f85
fracchiooo <sconocchiapisoni.1889241@studenti.uniroma1.it>
2022-04-26 18:55:33 +0200

    cookies

--- server.js
+++ server.js
@@ -35,18 +35,23 @@ client.connect(function(err) {
 app.get('/', function(req, res) {
     console.log('get /');
     var cookies = cookie.parse(req.headers.cookie || '');
-    var email_cookie = cookies.email_cookie;
-    var profilo_cookie= cookies.profile_cookie;
-    console.log(email_cookie+' '+profilo_cookie);
+
+    var cookies = cookies.email_profilo_cookie;
 
 
-    if (email_cookie) {
-        console.log("mando i cookie");
-        res.render('index.ejs', { data: database, cookie:email_cookie, profilo:profilo_cookie });
 
-    } 
-    else {
-        console.log("non mando i cookie");
+
+    if (cookies) {
+        var temp = cookies.split(',');
+        var email_cookie = temp[0];
+        var profilo_cookie = temp[1];
+        console.log("ho ricevuto i cookie...");
+        console.log(email_cookie + ' ' + profilo_cookie);
+
+        res.render('index.ejs', { data: database, cookie: email_cookie, profilo: profilo_cookie });
+
+    } else {
+        console.log("non ho ricevuto i cookie");
         res.render('index.ejs', { data: database, profilo: '' });
     }
 });
@@ -57,11 +62,11 @@ app.get('/offerte', function(req, res) {
     var cookies = cookie.parse(req.headers.cookie || '');
 
     var email_cookie = cookies.email_cookie;
-    var profilo_cookie= cookies.profile_cookie;
+    var profilo_cookie = cookies.profile_cookie;
 
     if (email_cookie) {
         console.log("mando i cookie");
-        res.render('offerte.ejs', { data: database, cookie:email_cookie, profilo:profilo_cookie });
+        res.render('offerte.ejs', { data: database, cookie: email_cookie, profilo: profilo_cookie });
     } else {
         console.log("non mando i cookie");
         res.render('offerte.ejs', { data: database, profilo: '' });
@@ -81,7 +86,7 @@ app.get('/offerta', urlencodedParser, function(req, res) {
     res.render('titolo.ejs', { offerta: database[req.query.id] });
 
 });
-app.get('/navbar',function(req, res) {
+app.get('/navbar', function(req, res) {
     console.log('get /navbar');
     res.render('navbar.ejs');
 
@@ -139,18 +144,18 @@ app.post('/login', urlencodedParser, function(req, res) {
         if (error) { console.log(error); return; }
 
         console.log("Utente loggato correttamente");
-          try {
-                profilo = String(result.rows[0].foto_profilo);
-                console.log(profilo);
-                //res.render('index.ejs', { data: database, profilo: p });
-    
-            } catch (error) {
-                console.log(error);
-            }
-        
+        try {
+            profilo = String(result.rows[0].foto_profilo);
+            console.log(profilo);
+            //res.render('index.ejs', { data: database, profilo: p });
+
+        } catch (error) {
+            console.log(error);
+        }
+
 
     });
-    setTimeout(function(){
+    setTimeout(function() {
         //qua setto i cookie
         //var query = url.parse(req.url, true, true).query;
         var email_cookie;
@@ -159,22 +164,25 @@ app.post('/login', urlencodedParser, function(req, res) {
         try {
             email_cookie = req.body.email_cookie;
             profile_cookie = req.body.profile_cookie;
-            console.log(email_cookie+' '+profile_cookie);
+            console.log(email_cookie + ' ' + profile_cookie);
         } catch (error) {
             console.log(error);
         }
 
         if (!email_cookie) {
-            console.log("Procedo a settare i cookies: "+mail+ ' '+profilo);
+
+            console.log("Procedo a settare i cookies: " + mail + ' ' + profilo);
+
+
             // Set a new cookie with the name
-            res.setHeader('Set-email-Cookie', cookie.serialize('email_cookie', mail, {
-                                                                                httpOnly: true,
-                                                                                maxAge: 60 * 60 // 1 hour
-                                                                                }));
-            res.setHeader('Set-profile-Cookie', cookie.serialize('profile_cookie', profilo, {
-                                                                                httpOnly: true,
-                                                                                maxAge: 60 * 60 // 1 hour
-                                                                                }));
+            res.setHeader('Set-Cookie', cookie.serialize('email_profilo_cookie', [mail, profilo], {
+                httpOnly: true,
+                maxAge: 60 * 60 // 1 hour
+            }));
+            /*   res.setHeader('Set-Cookie', cookie.serialize('profile_cookie', profilo, {
+                   httpOnly: true,
+                   maxAge: 60 * 60 // 1 hour
+               }));*/
             // Redirect back after setting cookie
             res.statusCode = 302;
             res.setHeader('Location', req.headers.referer || '/');
@@ -183,7 +191,7 @@ app.post('/login', urlencodedParser, function(req, res) {
         res.end();
         return;
 
-    }, 500 );
+    }, 500);
 });
 
 app.post('/signin', urlencodedParser, function(req, res) {
@@ -194,7 +202,7 @@ app.post('/signin', urlencodedParser, function(req, res) {
     var cognome = req.body.lastName;
     var mail = req.body.email;
     var pass = md5(req.body.password);
-    
+
 
     client.query('insert into utente(email,password,storico_offerte,foto_profilo,nome,cognome) values (' + '\'' + mail + '\',' + '\'' + pass + '\',' + '\'{}\',' + '\'' + String('https://cdn.calciomercato.com/images/2019-05/Whatsapp.senza.immagine.2019.1400x840.jpg') + '\',' + '\'' + nome + '\',' + '\'' + cognome + '\');', function(error, result) {
 
@@ -209,8 +217,7 @@ app.post('/signin', urlencodedParser, function(req, res) {
             } else {
                 throw error;
             }
-        }
-        else{
+        } else {
             res.send("<p>Registrazione eseguita correttamente, clicca <a href='/'>qui<a> per tornare all'homepage </p> ");
         }
 
